2c7e6112dd44e0d1ef22d6e5d28ec8fee09954f7
shekarramaswamy4 <shekar.ram.428@gmail.com>
2019-10-30 13:52:22 -0400

    more probe training code

--- probe/probe_handler.py
+++ probe/probe_handler.py
@@ -1,18 +1,50 @@
+import torch 
+
 from probe import Probe
 
 class ProbeHandler():
-    def __init__(self, num_state_variables, is_supervised = False):
-        self.num_state_variables = num_state_variables
-        self.is_supervised = is_supervised
+	def __init__(self, num_state_variables, is_supervised = False):
+		self.num_state_variables = num_state_variables
+		self.is_supervised = is_supervised
 
-        self.probes = {}
-    
-    def setup_probes(self):
-        if self.is_supervised:
-            print('Implement fully supervised probe')
-        else:
-            for i in range(self.num_state_variables):
-                self.probes[i] = Probe()
+		self.probes = []
+		self.optimizers = []
+	
+	def setup_probes(self):
+		if self.is_supervised:
+			print('TODO: Implement fully supervised probe')
+		else:
+			for i in range(self.num_state_variables):
+				self.probes.append(Probe())
+		
+		for i in range(self.num_state_variables):
+			self.optimizers.append(torch.optim.Adam(list(self.probes[k].parameters()), lr=3e-4)) # Karpathy's constant
+		
+		# TODO: add LR schedulers w warmup and cycles
+	
+	def train_one_epoch(self, train_episodes, train_labels):
+		# TODO: randomize input
+		# for each example, loop through the encoders and compute loss
+		for j in range(self.num_state_variables):
+			cur_probe = self.probes[j]
+			cur_optim = self.optimizers[j]
+			cur_optim.zero_grad()
 
-    def train(self):
-        self.setup_probes()
+			# get label relevant to current probe
+
+			# make predictions
+			# get loss
+
+			# loss.backward()
+			# cur_optim.step()
+
+	def train(self, train_episodes, train_labels, \
+						val_episodes, val_labels, \ 
+						test_episodes, test_labels, epochs = 100):
+		print('--- Training Probes ---')
+		self.setup_probes()
+
+		for i in range(epochs):
+			print('Epoch: ' + str(i) + ' of ' + str(epochs))
+			# 
+        
